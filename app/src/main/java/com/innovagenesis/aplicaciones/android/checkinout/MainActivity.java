23f533288dbc734eb0ce18ca3facd9423305102b
AlexisRoj <alexisroj2@gmail.com>
2017-06-14 22:45:55 -0500

    Antes de crear toolbar

--- app/src/main/java/com/innovagenesis/aplicaciones/android/checkinout/MainActivity.java
+++ app/src/main/java/com/innovagenesis/aplicaciones/android/checkinout/MainActivity.java
@@ -1,13 +1,17 @@
 package com.innovagenesis.aplicaciones.android.checkinout;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.Window;
 import android.widget.TextView;
 
+import com.innovagenesis.aplicaciones.android.checkinout.colores.AdminColor;
 import com.innovagenesis.aplicaciones.android.checkinout.fragments.PrincipalFragment;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,20 +19,35 @@ public class MainActivity extends AppCompatActivity {
     private TextView mTextMessage;
     private FloatingActionButton fab;
 
+    Window window;
+
+
+
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+            window = getWindow();
+
             switch (item.getItemId()) {
                 case R.id.navigation_home:
+
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+
+                        window.setStatusBarColor( // Cambia la barra
+                                ContextCompat.getColor(MainActivity.this,
+                                        AdminColor.COLOR_CLIENTE));
+
+                        window.setNavigationBarColor(ContextCompat.getColor(MainActivity.this,
+                                AdminColor.COLOR_CLIENTE));
+
+                    }
                     fab.setImageResource(R.drawable.ic_clientes);
 
                     return true;
-        /*        case R.id.navigation_dashboard:
 
-                    fab.setImageResource(R.drawable.ic_acuerdos);
-                    return true;*/
                 case R.id.navigation_notifications:
 
                     fab.setImageResource(R.drawable.ic_precios);
@@ -46,15 +65,12 @@ public class MainActivity extends AppCompatActivity {
 
 
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        //navigation.setBackgroundColor(ContextCompat.getColor(this,R.color.colorPrimary));
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
         fab = (FloatingActionButton) findViewById(R.id.fab);
-
         getSupportFragmentManager().beginTransaction()
                 .replace(R.id.content, new PrincipalFragment())
                 .commit();
-
 
     }
 
---
+++ app/src/main/java/com/innovagenesis/aplicaciones/android/checkinout/colores/AdminColor.java
@@ -0,0 +1,13 @@
+package com.innovagenesis.aplicaciones.android.checkinout.colores;
+
+import com.innovagenesis.aplicaciones.android.checkinout.R;
+
+/**
+ * Encargada de amistrar los colores
+ * Created by alexi on 14/06/2017.
+ */
+
+public class AdminColor {
+
+    public static int COLOR_CLIENTE = R.color.green_400;
+}
